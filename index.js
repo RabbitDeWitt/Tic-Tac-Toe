ca357bf00e189968b278eb3899cdd9c4e1e69a09
David Coelho <davidsocoelho@outlook.com>
2023-02-18 04:16:16 -0300

    Projeto finalized

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 const tiles = Array.from(document.querySelectorAll('.tile'));
 const currentPlayer = document.querySelector('.display-player');
+const announcer = document.querySelector('.announcer');
+const resetBtn = document.querySelector('.reset');
 
 let isXturn = true;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8]
+]
+
+let numberOfPlays = 0;
+
+
+function startGame(){
+  numberOfPlays = 0;
+  for(let tile of tiles){
+    tile.addEventListener('click', handleClick, {once: true});
+  }
+  resetBtn.addEventListener('click', resetGame, {once: true});
+}
+
+function endGame(isWin, currentPlayer) {
+  if(isWin){
+    announcer.innerHTML = `Player <span class = ${currentPlayer}> ${currentPlayer[currentPlayer.length - 1]}</span> Won!!!`;
+    for(let tile of tiles){
+      tile.removeEventListener('click', handleClick);
+    }
+  }else if(!isWin && numberOfPlays == 9){
+    announcer.innerHTML = `Draw`;
+  }
+  announcer.classList.remove('hide');
+}
+
+function resetGame() {
+  for(let tile of tiles){
+    tile.classList.remove('playerX');
+    tile.classList.remove('playerO');
+  }
+  announcer.classList.add('hide');
+  announcer.innerHTML = ``;
+  startGame();
+}
+
+function checkForWin(currentPlayer){
+  return winningCombinations.some(combination => {
+    return combination.every(index => {
+      return tiles[index].classList.contains(currentPlayer);
+    });
+  });
+}
 
 function placeMark(tile, classToAdd){
   tile.classList.add(classToAdd);
 }
 
-function swapPlayer(classToAdd){
+function swapPlayer(){
   isXturn = !isXturn;
   currentPlayer.classList.remove('playerX');
   currentPlayer.classList.remove('playerO');
@@ -17,16 +71,21 @@ function swapPlayer(classToAdd){
   }else{
     currentPlayer.classList.add('playerO');
   }
-
-  }
+}
 
 function handleClick(e){
   const tile = e.target;
   const classToAdd = isXturn ? 'playerX' : 'playerO';
+  numberOfPlays++;
   placeMark(tile, classToAdd);
-  swapPlayer(classToAdd);
+
+  if(numberOfPlays >= 5){
+    endGame(checkForWin(classToAdd), classToAdd);
+  }
+  
+  
+  swapPlayer();
 }
 
-for(let tile of tiles){
-  tile.addEventListener('click', handleClick, {once: true});
-}
+
+startGame()
